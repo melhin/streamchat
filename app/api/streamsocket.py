e3d4ce77384ae74e50957dc4f0e197c4bbb36bf9
groot <melhin444@gmail.com>
2020-09-16 16:44:03 +0200

    Cleaning up contexts

--- app/api/streamsocket.py
+++ app/api/streamsocket.py
@@ -17,12 +17,6 @@ import contextvars
 router = APIRouter()
 
 
-cvar_client_addr = contextvars.ContextVar('client_addr', default=None)
-cvar_chat_info = contextvars.ContextVar('chat_info', default=None)
-cvar_tenant = contextvars.ContextVar('tenant', default=None)
-cvar_redis = contextvars.ContextVar('redis', default=None)
-
-
 async def verify_user_for_room(chat_info):
     verified = True
     pool = await get_redis_pool()
@@ -30,14 +24,11 @@ async def verify_user_for_room(chat_info):
         logger.info('Redis connection failure')
         return False
     # check for duplicated user names
-    already_exists = await pool.sismember(cvar_tenant.get()+":users", cvar_chat_info.get()['username'])
+    already_exists = await pool.sismember(chat_info['users'], chat_info['username'])
     if already_exists:
         logger.error(chat_info['username'] +' user already_exists in ' + chat_info['room'])
         verified = False
-    # check for restricted names
-
-    # check for restricted rooms
-    # check for non existent rooms
+    # TODO: add more validation here
     # whitelist rooms
     if not chat_info['room'] in ALLOWED_ROOMS:
         verified = False
@@ -55,46 +46,8 @@ async def get_redis_pool():
         return None
 
 
-async def get_chat_history():
-    pass
-
-
-async def ws_send_moderator(websocket: WebSocket, chat_info: dict):
-    """
-    wait for new items on chat stream and
-    send data from server to client over a WebSocket
-
-    :param websocket:
-    :type websocket:
-    :param chat_info:
-    :type chat_info:
-    """
-    pool = await get_redis_pool()
-    streams = chat_info['room'].split(',')
-    latest_ids = ['$' for i in streams]
-    ws_connected = True
-    logger.info(streams, latest_ids)
-    while pool and ws_connected:
-        try:
-            events = await pool.xread(
-                streams=streams,
-                count=XREAD_COUNT,
-                timeout=XREAD_TIMEOUT,
-                latest_ids=latest_ids
-            )
-            for _, e_id, e in events:
-                e['e_id'] = e_id
-                await websocket.send_json(e)
-                #latest_ids = [e_id]
-        except ConnectionClosedError:
-            ws_connected = False
-
-        except ConnectionClosedOK:
-            ws_connected = False
-
-
 async def ws_send(websocket: WebSocket, chat_info: dict):
-    """
+    '''
     wait for new items on chat stream and
     send data from server to client over a WebSocket
 
@@ -102,7 +55,7 @@ async def ws_send(websocket: WebSocket, chat_info: dict):
     :type websocket:
     :param chat_info:
     :type chat_info:
-    """
+    '''
     pool = await get_redis_pool()
     latest_ids = ['$']
     ws_connected = True
@@ -112,7 +65,7 @@ async def ws_send(websocket: WebSocket, chat_info: dict):
             if first_run:
                 # fetch some previous chat history
                 events = await pool.xrevrange(
-                    stream=cvar_tenant.get() + ":stream",
+                    stream=chat_info['stream'],
                     count=NUM_PREVIOUS,
                     start='+',
                     stop='-'
@@ -124,7 +77,7 @@ async def ws_send(websocket: WebSocket, chat_info: dict):
                     await websocket.send_json(e)
             else:
                 events = await pool.xread(
-                    streams=[cvar_tenant.get() + ":stream"],
+                    streams=[chat_info['stream']],
                     count=XREAD_COUNT,
                     timeout=XREAD_TIMEOUT,
                     latest_ids=latest_ids
@@ -133,7 +86,6 @@ async def ws_send(websocket: WebSocket, chat_info: dict):
                     e['e_id'] = e_id
                     await websocket.send_json(e)
                     latest_ids = [e_id]
-            #logger.info('################contextvar ', cvar_tenant.get())
         except ConnectionClosedError:
             ws_connected = False
 
@@ -147,7 +99,7 @@ async def ws_send(websocket: WebSocket, chat_info: dict):
 
 
 async def ws_recieve(websocket: WebSocket, chat_info: dict):
-    """
+    '''
     receive json data from client over a WebSocket, add messages onto the
     associated chat stream
 
@@ -155,7 +107,7 @@ async def ws_recieve(websocket: WebSocket, chat_info: dict):
     :type websocket:
     :param chat_info:
     :type chat_info:
-    """
+    '''
 
     ws_connected = False
     pool = await get_redis_pool()
@@ -177,14 +129,12 @@ async def ws_recieve(websocket: WebSocket, chat_info: dict):
                 'uname': chat_info['username'],
                 'msg': data['msg'],
                 'type': 'comment',
-                'room': chat_info['room']
+                'room': chat_info['room'],
             }
-            logger.info(cvar_tenant.get())
-            await pool.xadd(stream=cvar_tenant.get() + ":stream",
+            await pool.xadd(stream=chat_info['stream'],
                             fields=fields,
                             message_id=b'*',
                             max_len=STREAM_MAX_LEN)
-            #logger.info('################contextvar ', cvar_tenant.get())
         except WebSocketDisconnect:
             await remove_room_user(chat_info, pool)
             await announce(pool, chat_info, 'disconnected')
@@ -202,67 +152,59 @@ async def ws_recieve(websocket: WebSocket, chat_info: dict):
 
 
 async def add_room_user(chat_info: dict, pool):
-    #added = await pool.sadd(chat_info['room']+":users", chat_info['username'])
-    added = await pool.sadd(cvar_tenant.get()+":users", cvar_chat_info.get()['username'])
-    return added
+    return await pool.sadd(chat_info['users'], chat_info['username'])
 
 
 async def remove_room_user(chat_info: dict, pool):
-    #removed = await pool.srem(chat_info['room']+":users", chat_info['username'])
-    removed = await pool.srem(cvar_tenant.get()+":users", cvar_chat_info.get()['username'])
-    return removed
+    return await pool.srem(chat_info['users'], chat_info['username'])
 
 
 async def room_users(chat_info: dict, pool):
-    #users = await pool.smembers(chat_info['room']+":users")
-    users = await pool.smembers(cvar_tenant.get()+":users")
+    users = await pool.smembers(chat_info['users'])
     logger.info(len(users))
     return users
 
 
 async def announce(pool, chat_info: dict, action: str):
-    """
+    '''
     add an announcement event onto the redis chat stream
-    """
+    '''
     users = await room_users(chat_info, pool)
     fields = {
         'msg': f"{chat_info['username']} {action}",
         'action': action,
         'type': 'announcement',
-        'users': ", ".join(users),
-        'room': chat_info['room']
+        'users': ', '.join(users),
+        'room': chat_info['room'],
     }
-    #logger.info(fields)
 
-    await pool.xadd(stream=cvar_tenant.get() + ":stream",
+    await pool.xadd(stream=chat_info['stream'],
                     fields=fields,
                     message_id=b'*',
                     max_len=STREAM_MAX_LEN)
 
 
 async def chat_info_vars(username: str = None, room: str = None):
-    """
+    '''
     URL parameter info needed for a user to participate in a chat
     :param username:
     :type username:
     :param room:
     :type room:
-    """
+    '''
     if username is None and room is None:
-        return {"username": str(uuid.uuid4()), "room": 'chat:1'}
-    return {"username": username, "room": room}
+        raise
+    return {
+        'username': username,
+        'room': room,
+        'users': room + ':users',
+        'stream': room + 'stream',
+    }
 
 
-@router.websocket("/ws")
+@router.websocket('/ws')
 async def websocket_endpoint(websocket: WebSocket,
                              chat_info: dict = Depends(chat_info_vars)):
-    #logger.info('request.hostname', websocket.url.hostname)
-    tenant_id = ":".join([websocket.url.hostname.replace('.', '_'),
-                          chat_info['room']])
-    cvar_tenant.set(tenant_id)
-    cvar_chat_info.set(chat_info)
-
-
     # check the user is allowed into the chat room
     verified = await verify_user_for_room(chat_info)
     # open connection
